c5e02bb8a9c934a8e705829780319702eb221b12
stephencockerill <stephen@typecode.com>
2023-02-11 12:49:38 -0500

    add procfile and new route

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,13 @@ app.get("/", (req:Request, res:Response):void => {
   res.send("<link rel='stylesheet' href='index.css'><script src='minesweeper.js'></script><h1>Hi Steeeve!!!!!!!</h1><div class='border'></div>")
 });
 
+app.get("/max/", (req:Request, res:Response):void => {
+  res.send("Hey there, Max ;)")
+});
+
 app.listen(PORT, ():void => {
   console.log(`Server Running here 👉 http://localhost:${PORT}`);
 });
 
 app.use(express.static('dist'));
 app.use(express.static('src'));
-
-
