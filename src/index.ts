0e6602b458892dafd399a20dabd7c5c3f1d378ab
maximilianfuller <maximilian.fuller@gmail.com>
2023-03-05 21:08:05 -0500

    MVP for networked gameplay works.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,120 @@
 import express, {Request,Response,Application} from 'express';
+import { WebSocketServer } from 'ws';
 
 const app:Application = express();
-const PORT = process.env.PORT || 8000;
+app.use(express.static('dist'));
+app.use(express.static('src'));
+const wss = new WebSocketServer({ port: 8080 });
 
-app.get("/", (req:Request, res:Response):void => {
+let games = new Map<string, any>()
+
+function createBoard() {
+  let NUM_ROWS = 9
+  let NUM_COLS = 9
+  let MAX_BOMBS = 10
+  let bombs : Array<number> = [];
+  for (let i = 0; i < MAX_BOMBS; i++) {
+    let bomb = -1;
+    do {
+      bomb = Math.floor(Math.random() * NUM_COLS * NUM_ROWS);
+    } while (bombs.includes(bomb));
+    bombs.push(bomb);
+  }
+
+  return {"bombs": bombs, "numRows": NUM_ROWS, "numCols": NUM_COLS};
+}
+
+function handleClientCreate(ws: any, url: string) {
+  if(!games.get(url)) {
+    games.set(url, {});
+  }
+  let game = games.get(url)!;
+  if (!game.p1) {
+    ws.id = 'p1';
+    game.p1 = {"ws": ws, "clickedCells": []};
+    console.log('p1 has started a game at ' + url);
+  } else if (!game.p2) {
+    ws.id = 'p2';
+    game.p2 = {"ws": ws, "clickedCells": []};
+    console.log('p2 has joined the game at ' + url);
+    game.board = createBoard();
+    updateClients(url);
+  }
+}
+
+function handleClientUpdate(ws: any, url: string, data: any) {
+  let game = games.get(url)!;
+  game[ws.id].clickedCells = JSON.parse(data);
+  let winner = maybeGetWinner(url);
+  if (winner) {
+    console.log("winner!" + winner);
+    game.winner = winner;
+  }
+  updateClients(url);
+}
+
+function maybeGetWinner(url: string) {
+  let game = games.get(url)!;
+  let allCells = new Set([...Array(game.board.numRows*game.board.numCols).keys()]);
+  let bombCells = new Set([...game.board.bombs]);
+  let nonBombCells = new Set([...allCells].filter(x => !bombCells.has(x)));
+  let players = ["p1", "p2"];
+  for(let i = 0; i < 2; i++) {
+    let player = players[i];
+    let other = players[(i+1)%2];
+    let clickedCells = new Set([...game[player].clickedCells]);
+    let unClickedNonBombCells = [...nonBombCells].filter(x => !clickedCells.has(x));
+    if (unClickedNonBombCells.length == 0) {
+      // All the cells cleared, you win!
+      return player;
+    }
+    let clickedBombCells = [...bombCells].filter(x => clickedCells.has(x));
+    if (clickedBombCells.length > 0) {
+      // A bomb was clicked, you lose.
+      return other;
+    }
+  }
+  return null;
+}
+
+function updateClients(url: string) {
+  let game = games.get(url)!;
+  let p1Data = {...game.board};
+  p1Data.enemyClickedCells = game.p2.clickedCells;
+  if (game.winner) {
+    p1Data.gameOverMessage = game.winner == "p1" ? "YOU WIN!" : "you lose";
+  }
+  game.p1.ws.send(JSON.stringify(p1Data));
+
+  let p2Data = {...game.board};
+  p2Data.enemyClickedCells = game.p1.clickedCells;
+  if (game.winner) {
+    p2Data.gameOverMessage = game.winner == "p2" ? "YOU WIN!" : "you lose";
+  }
+  game.p2.ws.send(JSON.stringify(p2Data));
+}
+
+// Setup websocket
+wss.on('connection', function connection(ws: any, req: any) {
+  handleClientCreate(ws, req.url);
+  ws.on('message', function message(data: any) {
+    handleClientUpdate(ws, req.url, data)
+  });
+  ws.on('error', console.error);
+
+});
+
+// Setup express server
+app.get("/game/:id", (req:Request, res:Response):void => {
   res.sendFile("index.html", {root: __dirname })
 });
 
-app.get("/max/", (req:Request, res:Response):void => {
-  res.send("Hey there, Steeeve ;)))")
+app.listen(8000, ():void => {
+  console.log(`Server Running here 👉 http://localhost:${8000}`);
 });
 
-app.listen(PORT, ():void => {
-  console.log(`Server Running here 👉 http://localhost:${PORT}`);
-});
 
-app.use(express.static('dist'));
-app.use(express.static('src'));
+
+
+
+
--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,9 +1,5 @@
-TILE_PIXEL_LENGTH = 16
-
-NUM_ROWS = 16
-NUM_COLS = 16
-MAX_BOMBS = 40
-NEIGHBOR_RELATIVE_COORDS = [
+TILE_PIXEL_LENGTH = 16;
+let NEIGHBOR_RELATIVE_COORDS = [
   [-1, -1],
   [-1, 0],
   [-1, 1],
@@ -14,95 +10,120 @@ NEIGHBOR_RELATIVE_COORDS = [
   [1, 1]
 ];
 
-let BOMBS = [];
-for (let i = 0; i < MAX_BOMBS; i++) {
-  BOMBS.push(Math.floor(Math.random() * NUM_COLS * NUM_ROWS));
+let url = document.URL.replace('http', 'ws').replace('8000', '8080');
+
+let gameOn = false;
+
+const webSocket = new WebSocket(url);
+webSocket.onmessage = (event) => {
+  let board = JSON.parse(event.data);
+  if (!gameOn) {
+    gameOn = true;
+    setupBoard(board);
+  }
+  if (board.gameOverMessage) {
+    alert(board.gameOverMessage);
+  }
+};
+
+function updateServer(board) {
+  let clickedCells = getClickedCells(board);
+  webSocket.send(JSON.stringify(clickedCells));
 }
 
-console.log(BOMBS);
+function getClickedCells(board) {
+  let clickedCoords = [];
+  for (let i = 0; i < board.numRows; i++) {
+    for (let j = 0; j < board.numCols; j++) {
+      if (isRevealed(j, i, board)) {
+        clickedCoords.push(j + i*board.numCols);
+      }
+    }
+  }
+  return clickedCoords;
+}
 
-function getNeighborCoords(x, y) {
+function getNeighborCoords(x, y, board) {
   let out = []
   for (let i = 0; i < NEIGHBOR_RELATIVE_COORDS.length; i++) {
     let c = NEIGHBOR_RELATIVE_COORDS[i][0]
     let r = NEIGHBOR_RELATIVE_COORDS[i][1]
     if (x == 0 && c == -1) continue;
-    if (x == NUM_COLS - 1 && c == 1) continue;
+    if (x == board.numCols - 1 && c == 1) continue;
     if (y == 0 && r == -1) continue;
-    if (y == NUM_ROWS - 1 && r == 1) continue;
+    if (y == board.numRows - 1 && r == 1) continue;
     out.push([x + c, y + r])
   }
   return out
 }
 
-function getBombNeighborCount(x, y) {
+function getBombNeighborCount(x, y, board) {
   let count = 0
-  let coords = getNeighborCoords(x, y);
+  let coords = getNeighborCoords(x, y, board);
   for (let i = 0; i < coords.length; i++) {
     let c = coords[i]
-    if (BOMBS.includes(c[0] + NUM_COLS*c[1])) {
+    if (board.bombs.includes(c[0] + board.numCols*c[1])) {
       count++;
     }
   }
   return count;
 }
 
-function isBomb(x, y) {
-  return BOMBS.includes(x + NUM_COLS * y);
+function isBomb(x, y, board) {  
+  return board.bombs.includes(x + (board.numCols * y));
 }
 
-function reveal(x, y) {
-  let id = x + NUM_COLS * y;
+function reveal(x, y, board) {
+  let id = x + board.numCols * y;
   $("#" + id).addClass("revealed");
-  console.log($("#" + id).attr("class"));
 
 }
 
-function isRevealed(x, y) {
-  let id = x + NUM_COLS * y;
+function isRevealed(x, y, board) {
+  let id = x + board.numCols * y;
   return $("#" + id).hasClass("revealed");
 }
 
-function revealDFS(x, y) {
-  if (isRevealed(x, y)) {
+function revealDFS(x, y, board) {
+  if (isRevealed(x, y, board)) {
     return;
   }
-  reveal(x, y);
+  reveal(x, y, board);
 
-  if (getBombNeighborCount(x, y) > 0) {
+  if (getBombNeighborCount(x, y, board) > 0) {
     return;
   }
 
-  let coords = getNeighborCoords(x, y);
+  let coords = getNeighborCoords(x, y, board);
   for(let i = 0; i < coords.length; i++) {
-    revealDFS(coords[i][0], coords[i][1])
+    revealDFS(coords[i][0], coords[i][1], board)
   }
 }
 
-$(document).ready(function() {
-  $("h1").click(function() {
-    $(this).hide();
-  });
-
-  for (let x = 0; x < NUM_COLS; x++) {
-    for (let y = 0; y < NUM_ROWS; y++) {
+function setupBoard(board) {
+  for (let y = 0; y < board.numRows; y++) {
+    for (let x = 0; x < board.numCols; x++) {
       let cell = $('<div>', {
-        id: NUM_COLS * y + x,
+        id: board.numCols * y + x,
         class: 'cell',
       });
       cell.css("left", TILE_PIXEL_LENGTH * x);
       cell.css("top", TILE_PIXEL_LENGTH * y);
-      if (isBomb(x, y)) {
-        cell.addClass("bomb")
+      if (isBomb(x, y, board)) {
+        cell.addClass("bomb");
       } else {
-        cell.addClass("c" + getBombNeighborCount(x, y))
+        cell.addClass("c" + getBombNeighborCount(x, y, board))
       }
       cell.click(function() {
         let id = parseInt($(this).attr("id"))
-        revealDFS(id % NUM_COLS, Math.floor(id / NUM_COLS));
+        revealDFS(id % board.numCols, Math.floor(id / board.numCols), board);
+        updateServer(board);
       })
       $('.border').append(cell);
     }
   }
+}
 
+$(document).ready(function() {
+ 
 });
