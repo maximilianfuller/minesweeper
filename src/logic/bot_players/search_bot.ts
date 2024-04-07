7eb3463b609b7cd3b18c61b4a792811f08f0dbbf
maximilianfuller <maximilian.fuller@gmail.com>
2024-04-07 11:32:26 -0400

    add searchbot template

---
+++ src/logic/bot_players/search_bot.ts
@@ -0,0 +1,118 @@
+import { BasePlayer, StartInfo} from '../player';
+import { Cell, CellType } from '../cell';
+import { Board } from '../board'
+
+import { assert } from 'console';
+
+/**
+ * Bot that searches for bombs and safe squares
+ */
+export class SearchBot extends BasePlayer{
+
+    private startInfo?: StartInfo;
+    private isGameOver: boolean = false;
+
+    // board state, updated when new information is learned, or when flags are placed.
+    private board: CellType[][] = [];
+
+    public constructor() {
+        super();
+    }
+
+    notifyStart(startInfo: StartInfo): void {
+        this.startInfo = startInfo;
+        for (let x = 0; x < startInfo.boardNumCols; x++) {
+            let col: CellType[] = []
+            for (let y = 0; y < startInfo.boardNumRows; y++) {
+                col.push(CellType.UNKNOWN);
+            }
+            this.board.push(col);
+        }
+        this.select(startInfo.startX, startInfo.startY);
+        this.makeRepeatedMoves();
+    }
+   
+    notifyGameUpdate(newCells: Cell[]): void {
+        newCells.forEach(c => {
+            this.board[c.x][c.y] = c.cellType;
+            assert(c.cellType != CellType.BOMB, "SimpleBot can't click on bombs")
+        });
+    }
+
+    // Iterate through 'fringe' squares or numbers squares that border on 
+    // unknown squares, and find 'freebies' or squares that can be clicked on 
+    // safely, or marked as bombs.
+    private makeMove(): void {
+        let fringeCoords: number[][] = []
+        for (let x = 0; x < this.startInfo!.boardNumCols; x++) {
+            for (let y = 0; y < this.startInfo!.boardNumRows; y++) {
+                if([CellType.UNKNOWN, CellType.FLAG].includes(this.board[x][y])) {
+                    continue;
+                }
+                if (!this.getNeighbors(x, y).map(c => this.board[c[0]][c[1]]).includes(CellType.UNKNOWN))  {
+                    continue;
+                }
+                fringeCoords.push([x, y])
+            }
+        }
+        for (let i = 0; i < fringeCoords.length; i++) {
+            let x = fringeCoords[i][0];
+            let y = fringeCoords[i][1];
+
+            let num = Number(this.board[x][y]);
+            let numBombNeighbors = this.getNeighbors(x, y)
+                .map(c => this.board[c[0]][c[1]] == CellType.FLAG ? Number(1) : Number(0))
+                .reduce((sum, current) => sum + current, 0);
+            let remainingBombs = num - numBombNeighbors;
+            let unknownNeighbors = this.getNeighbors(x, y).filter(c => this.board[c[0]][c[1]] == CellType.UNKNOWN)
+            assert(unknownNeighbors.length > 0, "Missing unknown neighbors.");
+
+            let c = unknownNeighbors[0];
+            if (remainingBombs == 0) {
+                this.select(c[0], c[1]);
+                return;
+            }
+            if (remainingBombs == unknownNeighbors.length) {
+                this.markFlag(c[0], c[1]);
+                this.board[c[0]][c[1]] = CellType.FLAG;
+                return;
+            }
+        }
+    }
+
+    private makeRepeatedMoves(): void {
+        if (this.isGameOver) {
+            return;
+        }
+        let x = Math.floor(Math.random()*this.startInfo!.boardNumCols);
+        let y = Math.floor(Math.random()*this.startInfo!.boardNumRows);
+        this.makeMove();
+        setTimeout(() => { this.makeRepeatedMoves() } , 100);
+    }
+
+    private getNeighbors(x: number, y: number): number[][] {
+        return Board.NEIGHBOR_RELATIVE_COORDS
+            .map(c => [c[0]+x, c[1]+y])
+            .filter(c => this.isInBounds(c[0], c[1]))
+    }
+
+    private isInBounds(x: number, y: number): boolean {
+        return x >= 0 && y >= 0 && x < this.startInfo!.boardNumCols && y < this.startInfo!.boardNumRows;
+    }
+
+    // Convert position in grid (single int, 'reading' direction) to [col, row]
+    private posToCoords(i: number): Array<number> {
+        let x = i%this.startInfo!.boardNumCols;
+        let y = Math.floor(i/this.startInfo!.boardNumRows);
+        return [x, y]
+    }
+
+    // Convert [col, row] to position in grid
+    private coordsToPos(x: number, y: number): number {
+        return y*this.startInfo!.boardNumCols + x;
+    }
+
+    notifyWin(): void { this.isGameOver = true;}
+    notifyLoss(): void { this.isGameOver = true;}
+
+}
